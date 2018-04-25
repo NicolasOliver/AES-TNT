9af1af832e67de723093d03af79b4fad99833089
Unknown <nicoo.oliver@gmail.com>
2018-04-25 16:01:08 +0200

    SubBytes

--- AES/aes.cpp
+++ AES/aes.cpp
@@ -1,6 +1,20 @@
 #include "aes.h"
 
+constexpr uint8_t Aes::S_BOX[];
+
+
 Aes::Aes()
 {
 
+}
+
+void Aes::SubBytes(uint8_t tab[4][4])
+{
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        for (uint8_t j = 0; j < 4; j++)
+        {
+            tab[i][j] = S_BOX[tab[i][j]];
+        }
+    }
 }
